8022c6786d4f8393d54d7ff4ba7c37d6f39cdcde
Georgios Karanikolas <gkaranikolas@arx.net>
2023-09-25 15:55:19 +0300

    [Home] Update calculator result

--- app/src/main/java/com/seijind/insulincalculator/ui/home/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/seijind/insulincalculator/ui/home/viewmodel/HomeViewModel.kt
@@ -27,21 +27,35 @@ class HomeViewModel @Inject constructor(
     }
 
     fun onResultClicked() {
+        val totalCarbs = calculateTotalCarbs()
+        val gi = uiState.value?.gi?.value?.toInt() ?: 0
+        val divider = uiState.value?.divider?.value?.toInt() ?: 0
+
+        val result = calculateResult(totalCarbs, gi, divider)
+        updateUiState(result)
+    }
+
+    private fun calculateTotalCarbs(): Double {
         var totalCarbs = uiState.value?.totalCarbs?.doubleValue ?: 0.0
         uiState.value?.selectedFoods?.forEach { food ->
             totalCarbs += food.carbs.doubleValue * food.grams.value.toInt()
         }
-        val gi = uiState.value?.gi?.value?.toInt() ?: 0
-        val divider = uiState.value?.divider?.value?.toInt() ?: 0
+        return totalCarbs
+    }
 
-        val result: Double = when (gi) {
-            in 0..69 -> customRound(totalCarbs / divider)
-            in 70..179 -> customRound(totalCarbs / divider)
+    private fun calculateResult(totalCarbs: Double, gi: Int, divider: Int): Double {
+        return when (gi) {
+            in 0..59 -> customRound(totalCarbs / divider) - 0.5
+            in 60..69, in 70..179 -> customRound(totalCarbs / divider)
             in 180..239 -> customRound(totalCarbs / divider) + 0.5
             in 240..299 -> customRound(totalCarbs / divider) + 1.0
-            in 300..360 -> customRound(totalCarbs / divider) + 1.5
+            in 300..359 -> customRound(totalCarbs / divider) + 1.5
+            in 360..Int.MAX_VALUE -> customRound(totalCarbs / divider) + 2.0
             else -> 0.0
         }
+    }
+
+    private fun updateUiState(result: Double) {
         uiState.value = uiState.value?.copy(result = mutableStateOf(result.toString()))
     }
 
