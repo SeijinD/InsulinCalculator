0ce1dee2c910fc912a9b5668a074f3ba83fb92b0
Georgios Karanikolas <gkaranikolas@arx.net>
2023-09-27 15:09:40 +0300

    [Home] Minor change

--- app/src/main/java/com/seijind/insulincalculator/ui/home/model/Foods.kt
+++ app/src/main/java/com/seijind/insulincalculator/ui/home/model/Foods.kt
@@ -7,43 +7,35 @@ object Foods {
     val foods = listOf(
         Food(
             name = mutableStateOf("Apple"),
-            carbs = mutableDoubleStateOf(0.5),
-            grams = mutableStateOf("")
+            carbs = mutableDoubleStateOf(0.5)
         ),
         Food(
             name = mutableStateOf("Watermelon"),
-            carbs = mutableDoubleStateOf(0.3),
-            grams = mutableStateOf("")
+            carbs = mutableDoubleStateOf(0.3)
         ),
         Food(
             name = mutableStateOf("Egg"),
-            carbs = mutableDoubleStateOf(0.5),
-            grams = mutableStateOf("")
+            carbs = mutableDoubleStateOf(0.5)
         ),
         Food(
             name = mutableStateOf("Watermelon Big"),
-            carbs = mutableDoubleStateOf(0.3),
-            grams = mutableStateOf("")
+            carbs = mutableDoubleStateOf(0.3)
         ),
         Food(
             name = mutableStateOf("Apple 2"),
-            carbs = mutableDoubleStateOf(0.5),
-            grams = mutableStateOf("")
+            carbs = mutableDoubleStateOf(0.5)
         ),
         Food(
             name = mutableStateOf("Watermelon Small"),
-            carbs = mutableDoubleStateOf(0.3),
-            grams = mutableStateOf("")
+            carbs = mutableDoubleStateOf(0.3)
         ),
         Food(
             name = mutableStateOf("Potatoes"),
-            carbs = mutableDoubleStateOf(0.5),
-            grams = mutableStateOf("")
+            carbs = mutableDoubleStateOf(0.5)
         ),
         Food(
             name = mutableStateOf("Potatoes with Cheese"),
-            carbs = mutableDoubleStateOf(0.3),
-            grams = mutableStateOf("")
+            carbs = mutableDoubleStateOf(0.3)
         )
     )
 }
